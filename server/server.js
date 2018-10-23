86b2b146757c6d0b753b0063ca6ba66da6a9f96d
Elran <elran3000@gmail.com>
2018-10-23 14:20:03 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('views'));
+
+app.get('/',(req,res) =>{
+	res.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Started at port ${port}`);
+});
+
+module.exports = {app};
